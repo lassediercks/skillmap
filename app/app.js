72bcb2977d2b5da60bbff97d694786430a2a54a6
Lasse Diercks <lasse@jimdo.com>
2015-07-06 02:00:35 +0200

    firebase connection setup

--- app/app.js
+++ app/app.js
@@ -1,2 +1,12 @@
+require("../bower_components/firebase/firebase.js");
 require("angular");
 require("./page/page.js");
+
+
+// var myDataRef = new Firebase('..');
+
+// function postTestData(){
+//   myDataRef.push('Hi');
+// };
+
+// postTestData();
